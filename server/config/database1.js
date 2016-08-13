4d9c8f1e9430aa7bd18e3a5ceef9f4ed10b6ad0e
David Lundgren <dealundgren@gmail.com>
2016-08-13 16:02:22 -0700

    set up for heroku

--- server/config/database1.js
+++ server/config/database1.js
@@ -1,10 +1,14 @@
 var Sequelize = require('sequelize');
-const secrets1 = require('../../secrets1.js');
+// const secrets1 = require('../../secrets1.js');
 
 
-var db1 = new Sequelize('discollectDB', 'jordan', secrets1.dbpassword, {
-  host: secrets1.mysql.host,
-  port: secrets1.mysql.port,
+// var db1 = new Sequelize('discollectDB', 'jordan', secrets1.dbpassword, {
+//   host: secrets1.mysql.host,
+//   port: secrets1.mysql.port,
+// });
+var db1 = new Sequelize('discollectDB', 'jordan', process.env.db1password, {
+  host: process.env.db1host,
+  port: process.env.db1port,
 });
 
 db1
@@ -14,5 +18,6 @@ db1
   .catch(function (err) {
     console.log('Unable to connect to the main database, sadly:', err);
   });
+
 
   module.exports = db1;
--- server/config/database2.js
+++ server/config/database2.js
@@ -1,10 +1,14 @@
 var Sequelize = require('sequelize');
-const secrets2 = require('../../secrets2.js');
+// const secrets2 = require('../../secrets2.js');
 
 
-var db2 = new Sequelize('discollectarchive', 'lundgren', secrets2.dbpassword, {
-  host: secrets2.mysql.host,
-  port: secrets2.mysql.port,
+// var db2 = new Sequelize('discollectarchive', 'lundgren', secrets2.dbpassword, {
+//   host: secrets2.mysql.host,
+//   port: secrets2.mysql.port,
+// });
+var db2 = new Sequelize('discollectarchive', 'lundgren', process.env.db2password, {
+  host: process.env.db2host,
+  port: process.env.db2port,
 });
 
 db2
