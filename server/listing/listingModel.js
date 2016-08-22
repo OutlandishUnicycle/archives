573d4f0ca7ceefa73d200748005906e7411cdfb8
David Lundgren <dealundgren@gmail.com>
2016-08-22 12:05:36 -0700

    cron-workers fixed with no sequelize issues, redeploy

--- server/listing/listingModel.js
+++ server/listing/listingModel.js
@@ -2,19 +2,19 @@ var Sequelize = require('sequelize');
 var db1 = require('../config/database1.js');
 
 var Listing = db1.define('Listing', {
-    title: Sequelize.STRING,
-    zipcode: Sequelize.INTEGER,
-    takerId: Sequelize.INTEGER,
-    giverId:Sequelize.INTEGER,
-    status: Sequelize.INTEGER,
-    picReference: Sequelize.STRING,
-    category: Sequelize.STRING,
-    description: Sequelize.STRING,
-    condition: Sequelize.INTEGER,
-    giverRating:Sequelize.INTEGER,
-    takerRating: Sequelize.INTEGER,
-    stateUSA: Sequelize.STRING,
-    coordinates: Sequelize.STRING,
+  title: {type: Sequelize.STRING(30)},
+  zipcode: Sequelize.INTEGER,
+  takerId: { type: Sequelize.INTEGER, defaultValue: null },
+  giverId: { type: Sequelize.INTEGER, defaultValue: null },
+  status: { type: Sequelize.INTEGER, allowNull: true, defaultValue: 0 },
+  picReference: Sequelize.STRING,
+  category: Sequelize.STRING,
+  description: Sequelize.STRING,
+  condition: Sequelize.INTEGER,
+  giverRating: { type: Sequelize.INTEGER, allowNull: true, defaultValue: 0 },
+  takerRating: { type: Sequelize.INTEGER, allowNull: true, defaultValue: 0 },
+  stateUSA: Sequelize.STRING,
+  coordinates: { type: Sequelize.STRING, defaultValue: '0, 0' },
 });
 
 Listing.sync({ force: false });
