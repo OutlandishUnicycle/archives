bd20d5ce29944db8ef1abfab437424385d74eab0
David Lundgren <dealundgren@gmail.com>
2016-08-22 10:39:23 -0700

    adjust model for archives

--- server/listing/archiveModel.js
+++ server/listing/archiveModel.js
@@ -2,16 +2,19 @@ var Sequelize = require('sequelize');
 var db = require('../config/database2.js');
 
 var Archive = db.define('Archive', {
-  title: Sequelize.STRING,
-  zipcode: Sequelize.INTEGER,
-  status: Sequelize.INTEGER,
-  condition: Sequelize.INTEGER,
-  category: Sequelize.STRING,
-  description: Sequelize.STRING,
-  giverId: Sequelize.INTEGER,
-  takerId: Sequelize.INTEGER,
-  takerRating: Sequelize.INTEGER,
-  giverRating: Sequelize.INTEGER
+    title: {type: Sequelize.STRING(30)},
+    zipcode: Sequelize.INTEGER,
+    takerId: { type: Sequelize.INTEGER, defaultValue: null },
+    giverId: { type: Sequelize.INTEGER, defaultValue: null },
+    status: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
+    picReference: Sequelize.STRING,
+    category: Sequelize.STRING,
+    description: Sequelize.STRING,
+    condition: Sequelize.INTEGER,
+    giverRating: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
+    takerRating: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
+    stateUSA: Sequelize.STRING,
+    coordinates: { type: Sequelize.STRING, defaultValue: '0, 0' },
 });
 
 Archive.sync({ force: false });
